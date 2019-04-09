1e4d39b0259dc94271b5476b10979342249a16e3
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-09 10:14:32 +0800

    initial commit

---
+++ decision.py
@@ -0,0 +1,25 @@
+print "Example 1: Greater-than and less-than comparisons"
+print 2 > 1 # true
+print 1 > 2 # false
+print 5 < 20 # true
+print 100 < 19 # false
+
+
+print "Example 2: Equality and non-equality comparisons"
+print 5 == 5 # true
+print "hello" == "hello" # true
+print 5 == 10 # false
+print 5 == '5' # what do you think will happen here? false
+print 7 != 10 # true
+print 7 != 7 # false
+
+
+print "Example 3: A demo of what you are about to learn"
+if 3 < 5:
+    print "Three is definitely smaller than 5!"
+
+if 10 > 20: 
+    print "Did you know that 10 is greater than 20!?"
+else:
+    print "20 is greater than 10" 
+    #prints 20 is greater than 10
